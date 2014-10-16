0b97f57f00c8c27ac7e49cce85747952f84d1d91
Will Han <xingheng907@hotmail.com>
2014-10-16 17:33:43 +0800

    Initial Commit

---
+++ JSONModel_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSONModel_Demo
+//
+//  Created by WeiHan on 10/16/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSONModel_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSONModel_Demo
+//
+//  Created by WeiHan on 10/16/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
